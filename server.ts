ba3aa061ae8bf90e3182223ea7937b1949643812
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-06-17 01:21:20 +0200

    init project ✨

---
+++ server.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import { Gpio } from 'onoff'
+
+// Initialisation GPIOs
+const probe1 = new Gpio(4, 'in')
+const probe2 = new Gpio(27, 'in')
+const probe3 = new Gpio(22, 'in')
+const probe4 = new Gpio(23, 'in')
+const probe5 = new Gpio(24, 'in')
+const probe6 = new Gpio(25, 'in')
+
+probe1.watch((err, value) => {
+    console.log('From proble1 : ', { value })
+    console.log('From proble1 : ', { err })
+})
+
+probe2.watch((err, value) => {
+    console.log('From proble2 : ', { value })
+    console.log('From proble2 : ', { err })
+})
+
+probe3.watch((err, value) => {
+    console.log('From proble3 : ', { value })
+    console.log('From proble3 : ', { err })
+})
+
+probe4.watch((err, value) => {
+    console.log('From proble4 : ', { value })
+    console.log('From proble4 : ', { err })
+})
+
+probe5.watch((err, value) => {
+    console.log('From proble5 : ', { value })
+    console.log('From proble5 : ', { err })
+})
+
+probe6.watch((err, value) => {
+    console.log('From proble6 : ', { value })
+    console.log('From proble6 : ', { err })
+})
+
+const isAgainActive = (probe: Gpio) => {
+    const probeState = probe.readSync()
+    if (probeState === 1) {
+        console.log('Send SMS !')
+        console.log('Write event in logbook !')
+    } else {
+        console.log('False alarm !')
+    }
+}
